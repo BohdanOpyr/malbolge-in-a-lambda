9054b507e16276b043293c9eb872a514cfce6504
rizerphe <bohdanopyr@gmail.com>
2021-05-28 21:20:22 +0300

    Packed mainloop into a list(iter())

--- malbolge.py
+++ malbolge.py
@@ -3,18 +3,5 @@ rotate = lambda i: i//3+(i%3)*(3**9)
 
 def mal(code):# TODO: invalid char filtering
     [(memory := [ord(x) for x in code]), (memory.append(crz(memory[-1], memory[-2])) for x in range(3**10-len(code))), (c := [0]), (a := [0]), (d := [0]),(running := [True])]
-#    runner = list(iter([running[0]][-1], False))# This is a basic while loop in a lambda
-    while running[0]:
-        None if 33 <= memory[c[0]] <= 126 else exit()
-        {
-            4: lambda: c.__setitem__(0, memory[d[0]]),
-            5: lambda: print(end=chr(a[0]%256)),
-            23: lambda: a.__setitem__(0, ord(input()[0])),# TODO: better input method according to spec
-            39: lambda: [memory.__setitem__(d[0], (new:=rotate(memory[d[0]], ))), a.__setitem__(0, new)],
-            40: lambda: d.__setitem__(0, memory[d[0]]),
-            62: lambda: [memory.__setitem__(d[0], (new:=crz(a[0],memory[d[0]]))), a.__setitem__(0, new)],
-            68: lambda: None,
-            81: lambda: running.__setitem__(0, False)
-        }.get((memory[c[0]]+c[0])%94, lambda: None)()
-        [memory.__setitem__(c[0], {0: 57, 19: 108, 38: 113, 57: 91, 76: 79, 1: 109, 20: 125, 39: 116, 58: 37, 77: 65, 2: 60, 21: 82, 40: 121, 59: 92, 78: 49, 3: 46, 22: 69, 41: 102, 60: 51, 79: 67, 4: 84, 23: 111, 42: 114, 61: 100, 80: 66, 5: 86, 24: 107, 43: 36, 62: 76, 81: 54, 6: 97, 25: 78, 44: 40, 63: 43, 82: 118, 7: 99, 26: 58, 45: 119, 64: 81, 83: 94, 8: 96, 27: 35, 46: 101, 65: 59, 84: 61, 9: 117, 28: 63, 47: 52, 66: 62, 85: 73, 10: 89, 29: 71, 48: 123, 67: 85, 86: 95, 11: 42, 30: 34, 49: 87, 68: 33, 87: 48, 12: 77, 31: 105, 50: 80, 69: 112, 88: 47, 13: 75, 32: 64, 51: 41, 70: 74, 89: 56, 14: 39, 33: 53, 52: 72, 71: 83, 90: 124, 15: 88, 34: 122, 53: 45, 72: 55, 91: 106, 16: 126, 35: 93, 54: 90, 73: 50, 92: 115, 17: 120, 36: 38, 55: 110, 74: 70, 93: 98, 18: 68, 37: 103, 56: 44, 75: 104}[memory[c[0]]%94]) if 33 <= memory[c[0]] <= 126 else None, c.__setitem__(0, (c[0]+1)%(3**10)), d.__setitem__(0, (d[0]+1)%(3**10))]
+    list(iter(lambda: [None if 33 <= memory[c[0]] <= 126 else exit(),{4: lambda: c.__setitem__(0, memory[d[0]]),5: lambda: print(end=chr(a[0]%256)),23: lambda: a.__setitem__(0, ord(input()[0])),39: lambda: [memory.__setitem__(d[0], (new:=rotate(memory[d[0]], ))), a.__setitem__(0, new)],40: lambda: d.__setitem__(0, memory[d[0]]),62: lambda: [memory.__setitem__(d[0], (new:=crz(a[0],memory[d[0]]))), a.__setitem__(0, new)],68: lambda: None,81: lambda: running.__setitem__(0, False)}.get((memory[c[0]]+c[0])%94, lambda: None)(),memory.__setitem__(c[0], {0: 57, 19: 108, 38: 113, 57: 91, 76: 79, 1: 109, 20: 125, 39: 116, 58: 37, 77: 65, 2: 60, 21: 82, 40: 121, 59: 92, 78: 49, 3: 46, 22: 69, 41: 102, 60: 51, 79: 67, 4: 84, 23: 111, 42: 114, 61: 100, 80: 66, 5: 86, 24: 107, 43: 36, 62: 76, 81: 54, 6: 97, 25: 78, 44: 40, 63: 43, 82: 118, 7: 99, 26: 58, 45: 119, 64: 81, 83: 94, 8: 96, 27: 35, 46: 101, 65: 59, 84: 61, 9: 117, 28: 63, 47: 52, 66: 62, 85: 73, 10: 89, 29: 71, 48: 123, 67: 85, 86: 95, 11: 42, 30: 34, 49: 87, 68: 33, 87: 48, 12: 77, 31: 105, 50: 80, 69: 112, 88: 47, 13: 75, 32: 64, 51: 41, 70: 74, 89: 56, 14: 39, 33: 53, 52: 72, 71: 83, 90: 124, 15: 88, 34: 122, 53: 45, 72: 55, 91: 106, 16: 126, 35: 93, 54: 90, 73: 50, 92: 115, 17: 120, 36: 38, 55: 110, 74: 70, 93: 98, 18: 68, 37: 103, 56: 44, 75: 104}[memory[c[0]]%94]) if 33 <= memory[c[0]] <= 126 else None, c.__setitem__(0, (c[0]+1)%(3**10)), d.__setitem__(0, (d[0]+1)%(3**10)), running[0]][-1], False))# TODO: better input method according to spec
 mal('''(=<`#9]~6ZY327Uv4-QsqpMn&+Ij"'E%e{Ab~w=_:]Kw%o44Uqp0/Q?xNvL:`H%c#DD2^WV>gY;dts76qKJImZkj''')
